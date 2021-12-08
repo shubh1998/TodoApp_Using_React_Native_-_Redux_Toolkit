96a3415fdbaf2a095037d342ab27b5f375988f80
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 22:53:00 +0530

    fix: update defaultRootStoreType

--- src/utils/types/defaultRootStoreType.ts
+++ src/utils/types/defaultRootStoreType.ts
@@ -1,8 +1,12 @@
 import { listType } from "./listType";
+import { taskType } from "./taskType";
 
 export type DefaultRootStoreType = {
     ListReducer: {
         list: Array<listType>,
         activeListId: string
+    },
+    TaskReducer: {
+        taskList: Array<taskType>,
     }
 }
