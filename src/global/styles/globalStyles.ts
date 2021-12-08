3afb0b0262d564b0595cc932c160585b5926b89a
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 15:14:35 +0530

    feat: added default global styles

---
+++ src/global/styles/globalStyles.ts
@@ -0,0 +1,42 @@
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export const globalStyles = {
+  loader: {
+    marginTop: 20,
+  },
+  noData: {
+    fontSize: 18,
+    fontFamily: 'Eina02-Regular',
+  },
+  listContainer: {
+    paddingHorizontal: 20,
+  },
+  listItem: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+    borderLeftWidth: 2,
+    borderLeftColor: Colors.primary,
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.quaternary,
+    borderStyle: 'solid',
+    fontFamily: 'Poppins-Light',
+    fontSize: 20,
+    lineHeight: 28,
+    marginBottom: 30,
+    padding: 5,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  switchText: {
+    fontSize: 18,
+    fontFamily: 'Eina02-Regular',
+  },
+};
