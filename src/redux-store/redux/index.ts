2941f3aca68dec9ebe01efac66cfd9304f2c465d
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 20:43:08 +0530

    feat: config taskReducer

--- src/redux-store/redux/index.ts
+++ src/redux-store/redux/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import ListReducer from "./List/ListReducer";
+import TaskReducer from "./Task/TaskReducer"
 
 export const rootReducer = combineReducers({
-    ListReducer
+    ListReducer,
+    TaskReducer
 });
