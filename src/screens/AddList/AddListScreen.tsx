fed4f16802bb6f6df44256eacb4587fa8ffe4c38
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 17:47:52 +0530

    feat: updated addListScreen

--- src/screens/AddList/AddListScreen.tsx
+++ src/screens/AddList/AddListScreen.tsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import { View, TouchableWithoutFeedback, StyleSheet, TextInput, Keyboard, Alert, ToastAndroid } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { CustomButton } from '../../components/Button/Button';
+import { globalStyles } from '../../global/styles/globalStyles';
+import { AddNewList } from '../../redux-store/redux/ToDoApp/ListReducer';
+import { DefaultRootStoreType } from '../../utils/types/defaultRootStoreType';
+import { listType } from '../../utils/types/listType';
 
-export const AddListScreen = () => {
-    return (
-        <View>
-            <Text>AddListScreen Screen</Text>
-        </View>
-    )
-}
+export const AddListScreen = ({
+    navigation,
+  }: {
+    navigation: StackScreenProps<any, any>['navigation'];
+  }) => {
+  const [newList, setNewList] = useState('');
+  const dispatch = useDispatch();
+  const lists: Array<listType> = useSelector(
+    (state: DefaultRootStoreType) => state.ListReducer.list,
+  );
+
+  const submitHandler = () => {
+    if (newList.trim() === '') {
+      return Alert.alert('Validation', 'Name is required!');
+    }
+    const alreadyExist = lists.find((l: listType) => l.note.toLowerCase() === newList.trim().toLowerCase());
+    if (alreadyExist) {
+      return Alert.alert('Validation', 'List with this name already exist!');
+    }
+    
+    const newListObject = {
+        id: Math.random(),
+        note: newList
+    }
+
+    dispatch(AddNewList(newListObject));
+    ToastAndroid.show(`"${newList}" list created!`, ToastAndroid.LONG);
+    navigation.navigate("Home")
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <TextInput style={globalStyles.input} value={newList} onChangeText={(val) => setNewList(val)} placeholder="List name" placeholderTextColor={Colors.tertiary} />
+        <CustomButton text="Submit" onPress={submitHandler} />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingTop: 50,
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+});
