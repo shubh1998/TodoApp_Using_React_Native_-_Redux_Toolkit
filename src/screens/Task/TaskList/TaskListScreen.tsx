f0ad69e129f577c59740566ccee2054a5866d0c5
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 18:42:13 +0530

    feat: added tasklist screen

---
+++ src/screens/Task/TaskList/TaskListScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const TaskListScreen = () => {
+    return (
+        <View>
+            <Text>Task List</Text>
+        </View>
+    )
+}
