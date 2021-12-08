6af8d877807cb791bc821899552700def10fcdc1
shubh1998 <shubhamgupta7440@gmail.com>
2021-12-08 19:08:46 +0530

    feat: Integrate deleteList action using AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -5,6 +5,11 @@ import { HomeScreen } from '../screens/Home/HomeScreen';
 import { AddListScreen } from '../screens/AddList/AddListScreen';
 import { Colors } from '../utils/constants';
 import { TaskListScreen } from '../screens/Task/TaskList/TaskListScreen';
+import { Alert, ToastAndroid, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useDispatch } from 'react-redux';
+import { deleteList } from '../redux-store/redux/ToDoApp/ListReducer';
+
 
 const TasksStackNavigator = createStackNavigator();
 
@@ -15,10 +20,44 @@ const defaultStyles = {
     headerTintColor: Colors.white,
     headerTitleStyle: {
         fontFamily: "Eina02-Regular"
+    },
+    deleteIconStyle: {
+      marginRight: 15
     }
 }
 
 const TaskNavigator = () => {
+  const dispatch = useDispatch()
+
+  const deleteListConfirm = ({
+    id, 
+    navigation
+  }:{
+    id: string | number, 
+    navigation: StackScreenProps<any, any>['navigation']
+  }) => {
+    dispatch(deleteList(id))
+    navigation.goBack();
+    ToastAndroid.show('List deleted successsfully!', ToastAndroid.LONG);
+  }
+
+  const onClickToDeleteList = ({
+    id, 
+    navigation
+  }:{
+    id: string | number, 
+    navigation: StackScreenProps<any, any>['navigation']
+  }) => {
+    Alert.alert(
+      'Delete List', 
+      "Are you sure you want to delete list", 
+      [
+        {text: 'Cancel'},
+        {text: 'Confirm Delete', onPress: ()=>deleteListConfirm({id, navigation})}
+      ]
+    )
+  }
+
   return (
     <TasksStackNavigator.Navigator>
       <TasksStackNavigator.Screen
@@ -36,7 +75,24 @@ const TaskNavigator = () => {
       <TasksStackNavigator.Screen
         name="TaskList"
         component={TaskListScreen}
-        options={({route, navigation}: {route: any, navigation: StackScreenProps<any, any>['navigation'] })=>({...defaultStyles, title: route.params.name})}
+        options={({route, navigation}: {
+          route: any, 
+          navigation: StackScreenProps<any, any>['navigation'] 
+        })=>(
+          {
+            ...defaultStyles, 
+            title: route.params.name,
+            headerRight: ()=>(
+              <View style={defaultStyles.deleteIconStyle}>
+                <Icon name="md-trash" 
+                  color="#fff" 
+                  size={30} 
+                  onPress={() => onClickToDeleteList({id: route.params.id, navigation})}
+                />
+              </View>
+            )
+          }
+        )}
       />
     </TasksStackNavigator.Navigator>
   );
